9e6fac7a4c6db3e809c6d6b483919eead650d0ca
falskuyyy <ahmdfalah10@gmail.com>
2023-03-27 10:35:18 +0700

    Add project files.

---
+++ exercise1/exercise1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+double rerata(double a, double b) {
+	return (a + b) / 2;
+}
+
+string status(double c) {
+	if (c >= 70)
+		return "Lulus";
+	else
+		return "Gagal";
+}
+
+string status2(double rata, double nil) {
+	if (rata >= 70 || nil >= 80)
+		return "Lulus";
+	else
+		return "Gagal";
+}
+
+int main() {
+	double nilM, nilB;
+	cout << "Masukkan nilai matematika = ";
+	cin >> nilM;
+		cout << "Masukkan nilai bahasa inggris = ";
+	cin >> nilB;
+	cout << "Status Kelulusan = " << status(rerata(nilM, nilB));
+	cout << "nStatus Kelulusan = " << status(rerata(nilM, nilB));
+	return 0;
+}
